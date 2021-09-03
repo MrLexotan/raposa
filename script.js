66f3663f86ab420b80403de507207d4331dd477a
MrLexotan <leite.tavares.joao@gmail.com>
2021-09-03 20:00:18 -0300

    Adicionado smooth scroll ao nav do site

--- script.js
+++ script.js
@@ -41,4 +41,25 @@ function initAccordion() {
 
 initAccordion()
 
+function initSmoothScroll() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    function scrollToSection(e) {
+        e.preventDefault()
+        const href = e.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
+    })
+
+}
+
+initSmoothScroll()
+
 
