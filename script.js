8fbb8a1f26b02c44667d2fce3ca1228641189e96
MrLexotan <leite.tavares.joao@gmail.com>
2021-09-04 23:55:25 -0300

    Adicionado animação ao usar o scroll na página

--- script.js
+++ script.js
@@ -62,4 +62,26 @@ function initSmoothScroll() {
 
 initSmoothScroll()
 
+function scrollAnimation() {
+    const sections = document.querySelectorAll('.js-scroll')
+    if (sections.length) {
+        const halfWindow = window.innerHeight * 0.6
 
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - halfWindow
+                if (sectionTop < 0) {
+                    section.classList.add('ativo')
+                }
+                else {
+                    section.classList.remove('ativo')
+                }
+            })
+        }
+
+        window.addEventListener('scroll', animaScroll)
+    }
+    animaScroll()
+}
+
+scrollAnimation()
